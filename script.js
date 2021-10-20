abbcf7bfe771821c2ebf00d4520ed5ffe2eebb1e
m1r1k25@mail.ru <m1r1k25@mail.ru>
2021-10-20 12:17:26 +0300

    add repository

---
+++ script.js
@@ -0,0 +1,81 @@
+const player = document.querySelector('.player');
+const playBtn = document.querySelector('.play');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const audio = document.querySelector('.audio');
+const progressContainer = document.querySelector('.progress__container');
+const progress = document.querySelector('.progress');
+const title = document.querySelector('.song');
+const cover = document.querySelector('.cover__img');
+const imgSrc = document.getElementById('img__src');
+
+const songs = ['The Pretender', 'Cant Stop', 'Stay With Me', 'Satisfaction'];
+
+let songIndex = 0; // песня по умолчанию
+
+function loadSong(song) {
+   title.innerHTML = song;
+   audio.src = `audio/${song}.mp3`;
+   cover.src = `img/cover${songIndex + 1}.jpg`;
+}
+loadSong(songs[songIndex]);
+
+function playSong() {
+   player.classList.add('play');
+   imgSrc.src = 'img/pause.png';
+   audio.play();
+   
+}
+
+function pauseSong() {
+   player.classList.remove('play');
+   imgSrc.src = 'img/play.png';
+   audio.pause();
+   
+}
+
+playBtn.addEventListener('click', () => {
+   const isPlaying = player.classList.contains('play');
+   if (isPlaying) {
+      pauseSong();
+   } else {
+      playSong();
+   }
+})
+
+function nextSong() {
+   songIndex++;
+   if (songIndex > songs.length - 1) {
+      songIndex = 0;
+   }
+   
+   loadSong(songs[songIndex]);
+   playSong();
+}
+
+nextBtn.addEventListener('click', nextSong);
+
+function prevSong() {
+   songIndex--;
+   if (songIndex < 0) {
+      songIndex = songs.length - 1;
+   }
+   loadSong(songs[songIndex]);
+   playSong();
+}
+
+prevBtn.addEventListener('click', prevSong);
+
+function updateProgress(event) {
+   const {duration, currentTime} = event.srcElement;
+   const progressPercent = (currentTime / duration) * 100;
+   progress.style.width = `${progressPercent}%`;
+   
+}
+audio.addEventListener('timeupdate', updateProgress)
+
+
+
+
+
+
